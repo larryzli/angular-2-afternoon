e9818b8e2fac4ffbd79894e8b4deb8e936ca78a9
Larry Li <larry.zy.li@gmail.com>
2018-01-31 14:48:51 -0600

    made default packages link show all packages

--- app/packages/packagesCtrl.js
+++ app/packages/packagesCtrl.js
@@ -3,10 +3,11 @@ angular
     .controller("packagesCtrl", function($scope, $stateParams, mainSrvc) {
         mainSrvc.getPackageInfo().then(response => {
             $scope.allPackages = response.data;
-            if ($stateParams.country) {
-                $scope.packages = $scope.allPackages.filter(package => {
-                    return package.country === $stateParams.country;
-                });
-            }
+            $scope.country = $stateParams.country;
+            // if ($stateParams.country) {
+            //     $scope.packages = $scope.allPackages.filter(package => {
+            //         return package.country === $stateParams.country;
+            //     });
+            // }
         });
     });
